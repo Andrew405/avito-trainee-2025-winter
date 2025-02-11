c12038dcaa4ba832b74dc5d082f9b390ad0a5348
Andrey Dederchuk <adederchuk@icloud.com>
2025-02-11 18:35:18 +0300

    Init database

--- cmd/avito-shop/main.go
+++ cmd/avito-shop/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Avito-trainee/internal/config"
+	"Avito-trainee/internal/db"
 	"log"
 )
 
@@ -11,7 +12,11 @@ func main() {
 		log.Fatalf("Can't load config %v\n", err)
 	}
 
-	// TODO: init db
+	dbConn, err := db.NewPostgresConnection(cfg.DatabaseURL)
+	if err != nil {
+		log.Fatalf("Can't connect to database: %v\n", err)
+	}
+	defer dbConn.Close()
 
 	// TODO: init services
 
--- internal/db/connection.go
+++ internal/db/connection.go
@@ -1,1 +1,25 @@
 package db
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresConnection(databaseURL string) (*sql.DB, error) {
+	dbConn, err := sql.Open("postgres", databaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	dbConn.SetMaxOpenConns(25)                 // Максимальное количество открытых соединений
+	dbConn.SetMaxIdleConns(25)                 // Максимальное количество простаивающих соединений
+	dbConn.SetConnMaxLifetime(5 * time.Minute) // Максимальное время жизни соединения
+
+	if err := dbConn.Ping(); err != nil {
+		return nil, err
+	}
+
+	return dbConn, nil
+}
